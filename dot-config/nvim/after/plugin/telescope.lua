082435f2da5725ac20968f27e9d8c83a5af40d58
jhong97 <johnhong.learn@gmail.com>
2023-11-01 20:46:58 -0400

    Update telescope livegrep to use top-level git directory as root

--- dot-config/nvim/after/plugin/telescope.lua
+++ dot-config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,24 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
+vim.keymap.set('n', '<leader>fl', telescope.live_grep, {})
+vim.keymap.set('n', '<leader>fg', telescope.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+	telescope.grep_string({ search = vim.fn.input("🔍 grep > ") })
 end)
-vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fo', telescope.oldfiles, {})
+
+function LIVE_GREP_FROM_PROJECT_ROOT()
+  local git_root = vim.fn.system('give rev-parse --show-toplevel')
+  if vim.v.shell_error == 0 then
+    git_root = string.gsub(git_root, '\n$', '')
+    telescope.live_grep({ cwd = git_root })
+  else
+    telescope.live_grep()
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>fg', [[:lua LIVE_GREP_FROM_PROJECT_ROOT()<CR>]], { noremap = true})
+
+
+
+
