8489aa2e7ac3abd1826e6bd00f3eb459308428c2
jhong97 <johnhong.learn@gmail.com>
2023-11-03 00:23:13 -0400

    Update telescope mappings, shorten directory name during live_grep, add .ignore file to ignore certain files during live_grep

--- dot-config/nvim/after/plugin/telescope.lua
+++ dot-config/nvim/after/plugin/telescope.lua
@@ -1,23 +1,34 @@
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fl', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fg', telescope.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-	telescope.grep_string({ search = vim.fn.input("🔍 grep > ") })
-end)
-vim.keymap.set('n', '<leader>fo', telescope.oldfiles, {})
+local default = require('telescope')
+local builtin = require('telescope.builtin')
+
+default.setup{
+  defaults = {
+		path_display = {
+      shorten = {
+        "smart",
+        len = 3,
+        exclude = { 2, -1 } -- to update 
+      },
+    },
+	}
+}
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 
 function LIVE_GREP_FROM_PROJECT_ROOT()
   local git_root = vim.fn.system('git rev-parse --show-toplevel')
   if vim.v.shell_error == 0 then
     git_root = string.gsub(git_root, '\n$', '')
-    telescope.live_grep({ cwd = git_root })
+    builtin.live_grep({ cwd = git_root })
   else
-    telescope.live_grep()
+    builtin.live_grep()
   end
 end
 
-vim.api.nvim_set_keymap('n', '<leader>fg', [[:lua LIVE_GREP_FROM_PROJECT_ROOT()<CR>]], { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>fs', [[:lua LIVE_GREP_FROM_PROJECT_ROOT()<CR>]], { noremap = true})
+
 
 
 
